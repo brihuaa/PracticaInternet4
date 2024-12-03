b196d4be4deffc93cdc04c7d6f67a8f295401110
brihuaa <adrianbrihuegasanchez20@gmail.com>
2024-12-03 13:48:23 +0100

    03/12 v7

--- main.ts
+++ main.ts
@@ -7,18 +7,19 @@ import { VehicleModel, Vehicle } from "./types.ts";
 import { schema } from "./schema.ts";
 import { resolvers } from "./resolvers.ts";
 
-const url = 'mongodb+srv://otheruser:123456aaabbbb@cluster0.loyvx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const client = new MongoClient(url);
+const MONGO_URL = Deno.env.get("MONGO_URL");
 
-const dbName = 'BDP4';
+if (!MONGO_URL) {
+  throw new Error("Please provide a MONGO_URL");
+}
 
-await client.connect();
-console.log('Connected successfully to server');
-const db = client.db(dbName);
+const mongoClient = new MongoClient(MONGO_URL);
+await mongoClient.connect();
+
+console.info("Connected to MongoDB");
 
 
 const mongoDB = mongoClient.db("Vehiculos");
-
 const VehiclesCollection = mongoDB.collection<VehicleModel>("Vehiculos");
 
 const server = new ApolloServer({
@@ -27,8 +28,7 @@ const server = new ApolloServer({
   });
 
   const { url } = await startStandaloneServer(server, {
-    context: async () => ({ DinosaursCollection }),
+     context: async () => ({ VehiclesCollection }),
   });
   
   console.info(`Server ready at ${url}`);
- 
